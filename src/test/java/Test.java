7be0653930b3b200515c6cbe3525235274a3a3aa
praveenpolam100 <80611426+praveenpolam100@users.noreply.github.com>
2024-02-27 09:35:59 -0600

    initial files

---
+++ src/test/java/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public static int add(int a, int b){
+
+        return a+b;
+    }
+    public static int add(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public static void main(String[] args) {
+
+        int sum = Test.add(10, 20);
+        System.out.println(sum);
+       int sum1 =  Test.add(10, 20, 30);
+       System.out.println(sum1);
+    }
+}
