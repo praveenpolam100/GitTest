e09c672eab938816ad021d9bcb10b5f2ea48bd3d
praveenpolam100 <80611426+praveenpolam100@users.noreply.github.com>
2024-07-21 13:33:40 -0500

    add function parameter has been changed to 40 from 30

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -16,7 +16,7 @@ public class Test {
 
         int sum = Test.add(10, 20);
         System.out.println(sum);
-       int sum1 =  Test.add(10, 20, 30);
+       int sum1 =  Test.add(10, 20, 40);
        System.out.println(sum1);
     }
 }
