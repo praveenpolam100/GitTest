d90ab73c71226bfbadc3f54c4621f1477d0ff591
praveenpolam100 <80611426+praveenpolam100@users.noreply.github.com>
2024-07-22 16:03:04 -0500

    updated file randomTest

--- src/test/java/RandomTest.java
+++ src/test/java/RandomTest.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,24 +6,56 @@ public class RandomTest {
 
     public static void randomNumberGenerator(){
 
-        try{
-             Scanner scanner = new Scanner(System.in);
-              int inputNumber = scanner.nextInt();
-        }catch(InputMismatchException me){
-            //return exception and continue
+        int inputNumber=0;
+        int attempts=0;
 
+
+
+        try{
+            Random random =  new Random();
+            int randomNumber = random.nextInt(100)+1;
+
+            System.out.println(randomNumber);
+             //Scanner scanner = new Scanner(System.in);
+
+
+
+            while(randomNumber!=inputNumber){
+
+                Scanner scanner = new Scanner(System.in);
+                if(scanner.hasNextInt()) {
+                    inputNumber = scanner.nextInt();
+                    attempts++;
+                }else {
+                    System.out.println("Enter Number only.");
+                }
+
+
+                if (inputNumber== randomNumber){
+                    System.out.println("Success");
+                }
+                if(inputNumber< randomNumber){
+                    System.out.println("Guess higher Number");
+                }
+                if(inputNumber> randomNumber){
+                    System.out.println("Guess Lower Number");
+                }
+                if(attempts>10){
+                    System.out.println("Exceeded number of attempts:" +attempts);
+                    break;
+                }
+
+            }
+            System.out.println("Number of attempts made:" + attempts);
+
+        }catch(InputMismatchException me){
+            throw new InputMismatchException("Enter Number only");
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
             
-       
-
-        System.out.println(inputNumber);
-
-        Random random =  new Random();
-        int randomNumber = random.nextInt(100)+1;
-
-        System.out.println(randomNumber);
-
-    }
+   }
 
     public static void main(String[] args) {
         randomNumberGenerator();
