b55020307e9fb6e65dee5eee4b26276d1ea5d3de
praveenpolam100 <80611426+praveenpolam100@users.noreply.github.com>
2024-07-29 00:15:18 -0500

    updated file with random

--- src/test/java/ChangeControl.java
+++ src/test/java/ChangeControl.java
@@ -1,10 +1,14 @@
 import java.io.InputStream;
+import java.util.Random;
 
 public class ChangeControl {
 
     public static void main(String[] args) {
 
+        Random random = new Random();
+        int randomNumber = random.nextInt(100)+1;
 
+        System.out.println(randomNumber);
 
     }
 }
