e26f7bfc72514996729526d465bfc19a034147aa
praveenpolam100 <80611426+praveenpolam100@users.noreply.github.com>
2024-07-29 00:21:15 -0500

    Revert "initial file"
    This reverts commit 7886f62fce59b7fc2de1bbebe365ff208283ca8b.
    
    # Conflicts:
    #	src/test/java/ChangeControl.java

--- src/test/java/ChangeControl.java
+++ src/test/java/ChangeControl.java
@@ -1,14 +1,10 @@
 import java.io.InputStream;
-import java.util.Random;
 
 public class ChangeControl {
 
     public static void main(String[] args) {
 
-        Random random = new Random();
-        int randomNumber = random.nextInt(100)+1;
 
-        System.out.println(randomNumber);
 
     }
 }
